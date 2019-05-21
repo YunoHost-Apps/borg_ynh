c01074739e090e30fdeee0815aa1084f213278cb
ljf (zamentur) <zamentur@users.noreply.github.com>
2019-05-21 16:36:47 +0200

    [fix] Use pip instead backport (#32)
    * [fix] Use pip instead backport
    
    * [fix] Don't remove backport in remove script
    
    * [fix] Remove backport in restore script
    
    * [fix] Replace backports with pip during upgrade
    
    * [fix] Update version number
    
    * [fix] Remove backports if exists
    
    * [enh] Delete dead code
    
    * [fix] Remove backports if they exists
    
    * [fix] Don't install backports

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -190,13 +190,8 @@ ynh_check_app_version_changed () {
   echo $return_value
 }
 
-
-ynh_install_backports () {
-    echo "deb http://httpredir.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/$app-stretch-backports.list
-}
-
 ynh_remove_backports () {
-    rm /etc/apt/sources.list.d/$app-stretch-backports.list
+    rm -f /etc/apt/sources.list.d/$app-stretch-backports.list
 }
 
 ynh_debian_release () {
