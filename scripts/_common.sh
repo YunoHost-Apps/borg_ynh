47bb88d51efb3e2705d305f0f245f93055a3b3fb
ericgaspar <junk.eg@free.fr>
2020-07-15 21:12:00 +0200

    upgrade to v1.1.13

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,13 +12,12 @@ pkg_dependencies="python3-pip python3-dev libacl1-dev libssl-dev liblz4-dev pyth
 install_borg_with_pip () {
     if [ ! -d /opt/borg-env ]; then
         virtualenv --python=python3 /opt/borg-env
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install borgbackup==1.1.10
+        /opt/borg-env/bin/python /opt/borg-env/bin/pip install borgbackup==1.1.13
         echo "#!/bin/bash
     /opt/borg-env/bin/python /opt/borg-env/bin/borg \"\$@\"" > /usr/local/bin/borg
         chmod u+x /usr/local/bin/borg
     fi
 }
-
 
 #=================================================
 # COMMON HELPERS
@@ -48,22 +47,7 @@ ynh_save_args () {
     done
 }
 
-# Render templates with Jinja2
-#
-# Attention : Variables should be exported before calling this helper to be
-# accessible inside templates.
-#
-# usage: ynh_render_template some_template output_path
-# | arg: some_template - Template file to be rendered
-# | arg: output_path   - The path where the output will be redirected to
-ynh_render_template() {
-   local template_path=$1
-   local output_path=$2
-   # Taken from https://stackoverflow.com/a/35009576
-   python3 -c 'import os, sys, jinja2; sys.stdout.write(
-                    jinja2.Template(sys.stdin.read()
-                 ).render(os.environ));' < $template_path > $output_path
-}
+
 
 ynh_configure () {
     ynh_backup_if_checksum_is_different $2
@@ -71,22 +55,7 @@ ynh_configure () {
     ynh_store_file_checksum $2
 }
 
-# Remove any logs for all the following commands.
-#
-# usage: ynh_print_OFF
-# WARNING: You should be careful with this helper, and never forgot to use ynh_print_ON as soon as possible to restore the logging.
-ynh_print_OFF () {
-	set +x
-}
 
-# Restore the logging after ynh_print_OFF
-#
-# usage: ynh_print_ON
-ynh_print_ON () {
-	set -x
-	# Print an echo only for the log, to be able to know that ynh_print_ON has been called.
-	echo ynh_print_ON > /dev/null
-}
 
 # Send an email to inform the administrator
 #
@@ -145,62 +114,7 @@ $(yunohost tools diagnosis | grep -B 100 "services:" | sed '/services:/d')"
 	echo "$mail_message" | $mail_bin -a "Content-Type: text/plain; charset=UTF-8" -s "$mail_subject" "$recipients"
 }
 
-# Read the value of a key in a ynh manifest file
-#
-# usage: ynh_read_manifest manifest key
-# | arg: manifest - Path of the manifest to read
-# | arg: key - Name of the key to find
-ynh_read_manifest () {
-	manifest="$1"
-	key="$2"
-	python3 -c "import sys, json;print(json.load(open('$manifest', encoding='utf-8'))['$key'])"
-}
 
-
-# Checks the app version to upgrade with the existing app version and returns:
-# - UPGRADE_APP if the upstream app version has changed
-# - UPGRADE_PACKAGE if only the YunoHost package has changed
-#
-## It stops the current script without error if the package is up-to-date
-#
-# This helper should be used to avoid an upgrade of an app, or the upstream part
-# of it, when it's not needed
-#
-# To force an upgrade, even if the package is up to date,
-# you have to set the variable YNH_FORCE_UPGRADE before.
-# example: sudo YNH_FORCE_UPGRADE=1 yunohost app upgrade MyApp
-
-# usage: ynh_check_app_version_changed
-ynh_check_app_version_changed () {
-  local force_upgrade=${YNH_FORCE_UPGRADE:-0}
-  local package_check=${PACKAGE_CHECK_EXEC:-0}
-
-  # By default, upstream app version has changed
-  local return_value="UPGRADE_APP"
-
-  local current_version=$(ynh_read_manifest "/etc/yunohost/apps/$YNH_APP_INSTANCE_NAME/manifest.json" "version" || echo 1.0)
-  local current_upstream_version="${current_version/~ynh*/}"
-  local update_version=$(ynh_read_manifest "../manifest.json" "version" || echo 1.0)
-  local update_upstream_version="${update_version/~ynh*/}"
-
-  if [ "$current_version" == "$update_version" ] ; then
-      # Complete versions are the same
-      if [ "$force_upgrade" != "0" ]
-      then
-        echo "Upgrade forced by YNH_FORCE_UPGRADE." >&2
-        unset YNH_FORCE_UPGRADE
-      elif [ "$package_check" != "0" ]
-      then
-        echo "Upgrade forced for package check." >&2
-      else
-        ynh_die "Up-to-date, nothing to do" 0
-      fi
-  elif [ "$current_upstream_version" == "$update_upstream_version" ] ; then
-    # Upstream versions are the same, only YunoHost package versions differ
-    return_value="UPGRADE_PACKAGE"
-  fi
-  echo $return_value
-}
 
 ynh_debian_release () {
 	lsb_release --codename --short
