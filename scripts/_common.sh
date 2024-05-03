ea3f6e34920d821bf3309758d9f33cffa9ab3082
Félix Piédallu <felix@piedallu.me>
2024-05-03 15:31:50 +0200

    Move /opt/borg-env, /usr/local/bin/borg,backup-with-app to install_dir

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,26 +8,14 @@
 # PERSONAL HELPERS
 #=================================================
 
-# Install borg with pip if borg is not here
 install_borg_with_pip () {
-    BORG_VERSION=$(ynh_app_upstream_version)
+    ynh_exec_as "$app" python3 -m venv --upgrade "$install_dir/venv"
+    venvpy="$install_dir/venv/bin/python3"
 
-    if [ -d /opt/borg-env ]; then
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip list | grep "borgbackup *$BORG_VERSION" || ynh_secure_remove /opt/borg-env
-    fi
-    if [ ! -d /opt/borg-env ]; then
-        python3 -m venv /opt/borg-env
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install pip -U
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install setuptools -U
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install wheel -U
-        ynh_print_info --message="Installing/compiling borg, this may take some time..."
-        /opt/borg-env/bin/python /opt/borg-env/bin/pip install borgbackup[pyfuse3]==$BORG_VERSION
-        echo "#!/bin/bash
-    /opt/borg-env/bin/python /opt/borg-env/bin/borg \"\$@\"" > /usr/local/bin/borg
-        touch "/opt/borg-env/$(ynh_get_debian_release)"
-    fi
-    # We need this to be executable by other borg apps
-    chmod a+x /usr/local/bin/borg
+    ynh_exec_as "$app" "$venvpy" -m pip install --upgrade setuptools wheel
+
+    BORG_VERSION=$(ynh_app_upstream_version)
+    ynh_exec_as "$app" "$venvpy" -m pip install borgbackup[pyfuse3]=="$BORG_VERSION"
 }
 
 _gen_and_save_public_key() {
