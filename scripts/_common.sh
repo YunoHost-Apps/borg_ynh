af8e46ba9584d3d1e0014e9acba78bae7cac3b5b
Kayou <pierre@kayou.io>
2024-08-13 11:17:53 +0200

    Update scripts/_common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,7 +13,6 @@ install_borg_with_pip () {
     venvpy="$install_dir/venv/bin/python3"
 
     ynh_exec_as "$app" "$venvpy" -m pip install --upgrade setuptools wheel
-    
     BORG_VERSION=$(ynh_app_upstream_version)
     ynh_exec_as "$app" "$venvpy" -m pip install borgbackup[pyfuse3]=="$BORG_VERSION"
 }
