279e0c0d52e2e3f6f1627291b71e55c19f51b631
Florent <florent.git@zeteo.me>
2025-10-05 19:05:48 +0200

    Cleanup code

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-private_key_filename="id_${app}_ed25519"
-public_key_filename="id_${app}_ed25519.pub"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
@@ -20,18 +17,13 @@ install_borg_with_pip () {
     ynh_exec_as "$app" "$venvpy" -m pip install borgbackup[pyfuse3]=="$BORG_VERSION"
 }
 
-make_ssh_dir() {
-    install -o $app -g $app -m 0700 -d "$data_dir/.ssh"
-}
-
 _gen_and_save_public_key() {
     public_key=""
 
     if [[ -n "$server" ]]; then
-        make_ssh_dir
-        private_key="$data_dir/.ssh/$private_key_filename"
+        private_key="/root/.ssh/id_${app}_ed25519"
         if [ ! -f "$private_key" ]; then
-            ynh_exec_as "$app" ssh-keygen -q -t ed25519 -N "" -f "$private_key"
+            ssh-keygen -q -t ed25519 -N "" -f "$private_key"
         fi
         public_key=$(cat "$private_key.pub")
     fi
